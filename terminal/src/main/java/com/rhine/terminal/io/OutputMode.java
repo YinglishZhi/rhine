daa93af5733ddef089f5d765fca505e27ba06017
刘德志 <liudezhi03@meituan.com>
2020-02-22 14:54:29 +0800

    fix

--- terminal/src/main/java/com/rhine/terminal/io/OutputMode.java
+++ terminal/src/main/java/com/rhine/terminal/io/OutputMode.java
@@ -17,8 +17,30 @@ public class OutputMode implements Consumer<int[]> {
     @Override
     public void accept(int[] data) {
         if (readHandler != null && data.length > 0) {
-            readHandler.accept(new int[]{'\r', '\n'});
-            readHandler.accept(data);
+            int prev = 0;
+            int ptr = 0;
+            while (ptr < data.length) {
+                int cp = data[ptr];
+                if (cp == '\n') {
+                    if (ptr > prev) {
+                        sendChunk(data, prev, ptr);
+                    }
+                    readHandler.accept(new int[]{'\r', '\n'});
+                    prev = ++ptr;
+                } else {
+                    ptr++;
+                }
+            }
+            if (ptr > prev) {
+                sendChunk(data, prev, ptr);
+            }
         }
+    }
+
+    private void sendChunk(int[] data, int prev, int ptr) {
+        int len = ptr - prev;
+        int[] buf = new int[len];
+        System.arraycopy(data, prev, buf, 0, len);
+        readHandler.accept(buf);
     }
 }
